5e69b69c64b2fe2bf1306f8b2d775de1c9b1cd05
Alexandre Voyer <Alexandre.Voyer@outlook.com>
2016-07-10 02:14:55 +0000

    Replace Rspec's test older :should synthax with new :expect synthax.

--- spec/sentimentalizer_spec.rb
+++ spec/sentimentalizer_spec.rb
@@ -10,14 +10,14 @@ describe "Sentimentalizer" do
   end
 
   it "will return a valid ruby hash with a valid input" do
-    Sentimentalizer.analyze("I hate not tests").sentiment.should eq(":(")
+    expect(Sentimentalizer.analyze("I hate not tests").sentiment).to eq(":(")
   end
 
   it "will return a valid json string with a valid input" do
-    JSON.parse(Sentimentalizer.analyze("I hate not tests", true))["sentiment"].should eq(":(")
+    expect(JSON.parse(Sentimentalizer.analyze("I hate not tests", true))["sentiment"]).to eq(":(")
   end
 
   it "will assign a probability of .5 in the case of an unknown word" do
-    Sentimentalizer.analyze("a;lsdnzi").overall_probability.should eq(0.5)
+    expect(Sentimentalizer.analyze("a;lsdnzi").overall_probability).to eq(0.5)
   end
 end
